7d15a6a9a3642f6bfcd012d4668e6bfe66187c0d
FabMera <104039918+FabMera@users.noreply.github.com>
2023-08-28 08:09:57 -0400

    fin-app

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,16 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'shared-lazy-image',
-  templateUrl: './lazy-image.component.html',
-  styleUrls: ['./lazy-image.component.css']
+    selector: 'shared-lazy-image',
+    templateUrl: './lazy-image.component.html',
+    styleUrls: ['./lazy-image.component.css'],
 })
 export class LazyImageComponent implements OnInit {
+    constructor() {}
 
-      constructor() { }
+    @Input()
+    public url!: string;
 
-      @Input()
-      public url!: string;
-      ngOnInit(): void {
-      }
+    @Input()
+    public alt!: string;
+
+    public hasLoaded: boolean = false;
+
+    ngOnInit(): void {
+        if (!this.url) throw new Error('url is undefined');
+    }
+
+    public onLoad() {
+        setTimeout(() => {
+            this.hasLoaded = true;
+        }, 2000);
+        this.hasLoaded = true;
+    }
 }
